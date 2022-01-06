875b5ac8d929d724164f2d614a7127c3ee42c6b9
austin3410 <32914667+austin3410@users.noreply.github.com>
2022-01-06 00:38:09 -0600

    Improved the config and example-config layout.

--- config-example.py
+++ config-example.py
@@ -2,10 +2,14 @@ class PRODUCTION:
     def __init__(self) -> None:
         self.id = "PRODUCTION ID"
         self.token = "PRODUCTION TOKEN"
-        self.gif_token = "GIPHY TOKEN"
+        self = BOTH(self)
 
 class DEV:
     def __init__(self) -> None:
         self.id = "DEV ID"
         self.token = "DEV TOKEN"
-        self.gif_token = "GIPHY TOKEN"
+        self = BOTH(self)
+
+class BOTH:
+    def __init__(self, other_class) -> None:
+        other_class.gif_token = "GIPHY TOKEN"
