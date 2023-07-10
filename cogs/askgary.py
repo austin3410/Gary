7314a8a041f8f21639cd9cc0e0cc2bc3524ef875
Austin <austin.hohe@epcusa.com>
2023-07-10 14:11:41 -0500

    Increased max_tokens

--- cogs/askgary.py
+++ cogs/askgary.py
@@ -108,7 +108,7 @@ class AskGary(commands.Cog):
                         {"role": "user", "content": question},
                     ],
                     temperature=0.9,
-                    max_tokens=3500,
+                    max_tokens=7500,
                     top_p=1,
                     frequency_penalty=0.0,
                     presence_penalty=0.0
